2db0e650cb26281d88ca07a6cd3e7e6cbed89b75
Rene Alas <renea@laformasports.com>
2021-03-08 15:30:37 -0600

    Configuracion Inicial

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import classes from './Person.css';
+
+const person = props => {
+  console.log('[Person.js] rendering...');
+  return (
+    <div className={classes.Person}>
+      <p onClick={props.click}>
+        Hola mi Nombre es {props.name} y tengo {props.age} años!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
+};
+
+export default person;
